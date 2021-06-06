bffe64eafc2e75115f9be6e98061f7f949cf73fc
朱鸿光 <2634562735@qq.com>
2021-06-06 14:09:07 +0800

    hotfix commit3

--- src/main/java/com/demo/git/GitTest.java
+++ src/main/java/com/demo/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git demo");
         System.out.println("hot-fix modify modify");
+        System.out.println("hot-fix modify2");
     }
 }
